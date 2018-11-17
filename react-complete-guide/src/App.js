bffea90a45224c66f95a24e4351333365d918905
jvagnercs <jvagnercs@gmail.com>
2018-11-17 07:18:36 -0200

    31

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,9 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hi, I'm a React App
-        </h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
 
