efedff6fe057fd574d03c59f610ca758d2fc7023
jvagnercs <jvagnercs@gmail.com>
2018-11-17 07:33:51 -0200

    32 - added new component to render element, took semicolons out

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Person from './Person/Person'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <Person />
       </div>
     );
 
