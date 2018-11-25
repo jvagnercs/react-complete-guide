a296f22dfd134b9653d7da8acd2a89778c8743c8
jvagnercs <jvagnercs@gmail.com>
2018-11-25 11:16:50 -0200

    Lecture 71 - media query with css modules no eject

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -4,14 +4,8 @@ import Radium from 'radium'
 
 const person = props => {
 
-    const style = {
-        '@media (min-width: 500px)':{
-            width: '450px'
-        }
-    }
-
     return (
-    <div className={cssClasses.person} style={style}>
+    <div className={cssClasses.person} >
         <p onClick={props.click}>I'm {props.name} and I am {props.age} year old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
