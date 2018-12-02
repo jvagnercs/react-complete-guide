219f87b2fcc70e35d3e9d5d7401bafefbe86957f
jvagnercs <jvagnercs@gmail.com>
2018-12-02 14:52:05 -0200

    Lecture 81 - Splitting components

---
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import cssClasses from './Cockpit.module.css'
+
+
+const Cockpit = props => {
+    const classes = []
+
+    let btnClass = ''
+    if (props.showPersons) {
+        btnClass = cssClasses.Red
+    }
+
+    if (props.persons.length <= 2) {
+      classes.push( cssClasses.red )
+    }
+
+    if (props.persons.length <= 1) {
+      classes.push(cssClasses.bold)
+    }
+    
+    return <div className={cssClasses.Cockpit}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+        className={btnClass}
+        onClick={props.clicked}>Toggle Persons</button>
+    </div>
+}
+export default Cockpit
